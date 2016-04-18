d1d3447f4a35adaa4c5ed645aaf742843ffd64a9
chemzqm <chemzqm@gmail.com>
2016-04-18 14:42:10 +0800

    Update documentation

--- bundle.js
+++ bundle.js
@@ -63,7 +63,9 @@
 /* 1 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var styles = __webpack_require__(2)
+	var detect = __webpack_require__(2)
+	var transition = detect.transition
+	var styles = __webpack_require__(8)
 	var body = document.body
 	
 	/**
@@ -78,6 +80,8 @@
 	  if (el.style.position == 'absolute') throw new Error('element should not absolute positioned')
 	  var pel = getRelativeElement(el)
 	  var pos = getAbsolutePosition(el, pel)
+	  var transitionProp = styles(el, transition)
+	  el.style.transition = 'none'
 	  var orig = copy(el.style, {
 	    height: pos.height + 'px',
 	    width: pos.width + 'px',
@@ -85,6 +89,9 @@
 	    top: pos.top + 'px',
 	    position: 'absolute',
 	    float: 'none'
+	  })
+	  setTimeout(function () {
+	    el.style.transition = transitionProp
 	  })
 	  return function restore() {
 	    copy(el.style, orig)
@@ -140,6 +147,158 @@
 
 /***/ },
 /* 2 */
+/***/ function(module, exports, __webpack_require__) {
+
+	exports.transition = __webpack_require__(3)
+	
+	exports.transform = __webpack_require__(4)
+	
+	exports.touchAction = __webpack_require__(5)
+	
+	exports.transitionend = __webpack_require__(6)
+	
+	exports.has3d = __webpack_require__(7)
+
+
+/***/ },
+/* 3 */
+/***/ function(module, exports) {
+
+	var styles = [
+	  'webkitTransition',
+	  'MozTransition',
+	  'OTransition',
+	  'msTransition',
+	  'transition'
+	]
+	
+	var el = document.createElement('p')
+	var style
+	
+	for (var i = 0; i < styles.length; i++) {
+	  if (null != el.style[styles[i]]) {
+	    style = styles[i]
+	    break
+	  }
+	}
+	el = null
+	
+	module.exports = style
+
+
+/***/ },
+/* 4 */
+/***/ function(module, exports) {
+
+	
+	var styles = [
+	  'webkitTransform',
+	  'MozTransform',
+	  'msTransform',
+	  'OTransform',
+	  'transform'
+	];
+	
+	var el = document.createElement('p');
+	var style;
+	
+	for (var i = 0; i < styles.length; i++) {
+	  style = styles[i];
+	  if (null != el.style[style]) {
+	    module.exports = style;
+	    break;
+	  }
+	}
+
+
+/***/ },
+/* 5 */
+/***/ function(module, exports) {
+
+	
+	/**
+	 * Module exports.
+	 */
+	
+	module.exports = touchActionProperty();
+	
+	/**
+	 * Returns "touchAction", "msTouchAction", or null.
+	 */
+	
+	function touchActionProperty(doc) {
+	  if (!doc) doc = document;
+	  var div = doc.createElement('div');
+	  var prop = null;
+	  if ('touchAction' in div.style) prop = 'touchAction';
+	  else if ('msTouchAction' in div.style) prop = 'msTouchAction';
+	  div = null;
+	  return prop;
+	}
+
+
+/***/ },
+/* 6 */
+/***/ function(module, exports) {
+
+	/**
+	 * Transition-end mapping
+	 */
+	
+	var map = {
+	  'WebkitTransition' : 'webkitTransitionEnd',
+	  'MozTransition' : 'transitionend',
+	  'OTransition' : 'oTransitionEnd',
+	  'msTransition' : 'MSTransitionEnd',
+	  'transition' : 'transitionend'
+	};
+	
+	/**
+	 * Expose `transitionend`
+	 */
+	
+	var el = document.createElement('p');
+	
+	for (var transition in map) {
+	  if (null != el.style[transition]) {
+	    module.exports = map[transition];
+	    break;
+	  }
+	}
+
+
+/***/ },
+/* 7 */
+/***/ function(module, exports, __webpack_require__) {
+
+	
+	var prop = __webpack_require__(4);
+	
+	// IE <=8 doesn't have `getComputedStyle`
+	if (!prop || !window.getComputedStyle) {
+	  module.exports = false;
+	
+	} else {
+	  var map = {
+	    webkitTransform: '-webkit-transform',
+	    OTransform: '-o-transform',
+	    msTransform: '-ms-transform',
+	    MozTransform: '-moz-transform',
+	    transform: 'transform'
+	  };
+	
+	  // from: https://gist.github.com/lorenzopolidori/3794226
+	  var el = document.createElement('div');
+	  el.style[prop] = 'translate3d(1px,1px,1px)';
+	  document.body.insertBefore(el, null);
+	  var val = getComputedStyle(el).getPropertyValue(map[prop]);
+	  document.body.removeChild(el);
+	  module.exports = null != val && val.length && 'none' != val;
+	}
+
+
+/***/ },
+/* 8 */
 /***/ function(module, exports) {
 
 	// DEV: We don't use var but favor parameters since these play nicer with minification
